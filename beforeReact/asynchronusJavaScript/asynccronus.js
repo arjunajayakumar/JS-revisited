f7dc99f1d641a4351168b297a1cc8e78ea8a0a4c
arjun <arjune64@gmail.com>
2020-09-30 22:30:34 +0530

    async js

--- beforeReact/asynchronusJavaScript/asynccronus.js
+++ beforeReact/asynchronusJavaScript/asynccronus.js
@@ -18,38 +18,60 @@
 // 3. Async/Await
 
 // Callback
-console.log('Before');
-getUser(1, function (user) {
-    console.log(user);
-    // Get Repositories
-    getRepositories('Arjun', repo => {
-        getCommits(repo, (commits) => {
+// console.log('Before');
+// getUser(1, function (user) {
+//     console.log(user);
+//     // Get Repositories
+//     getRepositories('Arjun', repo => {
+//         getCommits(repo, (commits) => {
 
-        })
-    })
-})
-console.log('After');
+//         })
+//     })
+// })
+// console.log('After');
 
-// Synchronus
-console.log('Before');
-const user = getUser(1);
-const repos = getRepositories(user.gitHubUserName);
-const commits = getomits(repos[0])
+// // Synchronus
+// console.log('Before');
+// const user = getUser(1);
+// const repos = getRepositories(user.gitHubUserName);
+// const commits = getomits(repos[0]);
+// console.log('After')
 
-function getUser(id, callback) {
-    setTimeout(() => {
-        console.log('Reading a user from a database');
-        callback({ id: id, gutHubUsername: 'Arjun' });
-    }, 2000)
+// function getUser(id, callback) {
+//     setTimeout(() => {
+//         console.log('Reading a user from a database');
+//         callback({ id: id, gutHubUsername: 'Arjun' });
+//     }, 2000)
 
-}
+// }
 
-function getRepositories(username, callback) {
-    setTimeout(() => {
-        console.log('Here is the repositories of', username);
-        callback(['repo1', 'repo2', 'repo3'])
-    }, 2500)
+// function getRepositories(username, callback) {
+//     setTimeout(() => {
+//         console.log('Here is the repositories of', username);
+//         callback(['repo1', 'repo2', 'repo3'])
+//     }, 2500)
 
-}
+// }
 
 // In Asynchronus code nested callbacks makes weired structure & this is called callback hell
+
+// Named functions
+
+console.log('Before');
+getUser(1, getUser)
+    getRepositories('Arjun', getRepositories)
+        getCommits(repo, displayCommits)
+console.log('After');
+
+function displayCommits(commits) {
+    console.log(commits);
+}
+
+function getUser(user) {
+    console.log(User, user);
+}
+
+function displayComits(commits) {
+    console.log(commits);
+}
+
