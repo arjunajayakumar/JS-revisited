911f7572ab3143113044f9671cd7af824f1ec2f2
arjun <arjune64@gmail.com>
2020-10-06 22:08:35 +0530

    Operators:completed

---
+++ MOSH-JS/operators/operatorPrecedence.js
@@ -0,0 +1,7 @@
+let x = 2 + 3 * 4;
+// Output = 14;
+
+// In this case multiplication has higher precedence, if if we need to apply precedence we should put the expression in parenthesis
+
+let x = (2 + 3) * 4;
+// output = 20;
