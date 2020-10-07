0abc0adc04d62e531dc25128546dbafcdc2f317b
arjun <arjune64@gmail.com>
2020-10-07 13:23:55 +0530

    exercise 2

---
+++ MOSH-JS/control_flow/07_exercise.js
@@ -0,0 +1,13 @@
+// Max of two numbers
+// function max(a, b) {
+//  return (a > b) ? a : b;
+// }
+
+// console.log(max(7,3));
+
+// Landscape or Not
+function isLandscape(width, height) {
+    return (width > height);
+}
+
+console.log(isLandscape(800,600))
