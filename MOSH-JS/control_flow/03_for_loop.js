58453e02368657a95c08ca402dbd8c92834cb802
arjun <arjune64@gmail.com>
2020-10-07 12:09:47 +0530

    for loop

---
+++ MOSH-JS/control_flow/03_for_loop.js
@@ -0,0 +1,11 @@
+let num = [1, 2, 4, 5, 7, 8];
+
+
+for(let i = 0; i < num.length; i++) {
+//  if(num[i] + 1 !== num[i + 1])
+    // console.log('missing')
+    console.log('1:',(num[i] + 1));
+    console.log('2:',num[i + 1]);
+    
+}
+
