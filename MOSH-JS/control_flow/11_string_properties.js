a340d7d9f7e31b0138b17f2a294d0db3777cc1aa
arjun <arjune64@gmail.com>
2020-10-09 10:08:13 +0530

    exercise 10

---
+++ MOSH-JS/control_flow/11_string_properties.js
@@ -0,0 +1,19 @@
+let movie = {
+    name:'a',
+    releaseYear:2014,
+    rating:4.6,
+    director:'b'
+}
+
+stringProperties(movie)
+
+
+function stringProperties(obj) {
+    for(let value in obj) {
+        if(typeof(obj[value]) === "string" ) {
+            console.log(value, obj[value] );
+        }
+    }
+}
+
+
