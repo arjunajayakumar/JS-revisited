d46afea22a61a3e6e5b8a04bb7cef98c63be62f4
arjun <arjune64@gmail.com>
2020-11-02 21:42:57 +0530

    crossroads web dev challenge

--- js-fundementals/02_day02/01_execContext.js
+++ js-fundementals/02_day02/01_execContext.js
@@ -10,3 +10,34 @@ console.log(sayHello())
 
 let name = "Arjun"
 console.log(this)
+
+console.log('name: ', name)
+console.log('handle: ', handle)
+console.log('getUser: ', getUser)
+
+var name = "Tyler"
+var handle = "@tylermcginnis"
+
+function getUser() {
+    return {
+        name: name,
+        handle: handle
+    }
+
+}
+
+function first() {
+    var name = "Jordyn"
+    console.log(name)
+}
+
+function second() {
+    var name = "Jake"
+    console.log(name)
+}
+
+console.log(name)
+var name = "Tyler"
+first()
+second()
+console.log(name)
--- js-fundementals/02_day02/02_lexicalScoping.js
+++ js-fundementals/02_day02/02_lexicalScoping.js
@@ -1,30 +1,10 @@
-console.log('name: ', name)
-console.log('handle: ', handle)
-console.log('getUser: ', getUser)
-
-var name = "Tyler"
-var handle = "@tylermcginnis"
-
-function getUser() {
-    return {
-        name: name,
-        handle: handle
+function init() {
+    var firstName = "Arjun";
+    function sayFirstName() {
+        console.log(firstName)
     }
-
+    sayFirstName()
 }
 
-function first() {
-    var name = "Jordyn"
-    console.log(name)
-}
-
-function second() {
-    var name = "Jake"
-    console.log(name)
-}
-
-console.log(name)
-var name = "Tyler"
-first()
-second()
-console.log(name)
+init()
+console.log(firstName)
