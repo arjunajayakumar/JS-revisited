c68420862fb07eee2787e143e7473caa950b7ace
arjun <arjune64@gmail.com>
2020-11-08 22:40:56 +0530

    garbage collection

---
+++ MOSH-JS/05_objects/09_garbageCollection.js
@@ -0,0 +1,4 @@
+// In low level languages like console, c++ we need to allocate memmory for the variables. but in JS the JS engine automatically creates memmory and remove them when after use (Garbage collector)
+
+const obj = {}
+console.log(obj)
